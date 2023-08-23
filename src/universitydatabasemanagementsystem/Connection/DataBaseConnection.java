6da000cd34c0e705fa361a063c65ea86441f29e0
Muhammad Umair <142986548+EngrMuhammadUmair@users.noreply.github.com>
2023-08-23 16:14:04 +0500

    Add files via upload

---
+++ src/universitydatabasemanagementsystem/Connection/DataBaseConnection.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package universitydatabasemanagementsystem.Connection;
+
+/**
+ *
+ * @author M.Umair
+ */
+public class DataBaseConnection {
+    
+    public static String DriverPath = "net.ucanaccess.jdbc.UcanaccessDriver";
+    
+    public static String DatabaseFile = "jdbc:ucanaccess://D:\\BCE-3\\OOP\\UniversityDataBaseManagementSystem\\BahriaUniversityAccessDatabase.accdb";
+        
+}
